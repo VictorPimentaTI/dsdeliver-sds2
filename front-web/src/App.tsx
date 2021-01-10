1ee28a7633582d4aaea5100d44e35823116a52c4
Victor Pimenta <victorpimenta.ti@gmail.com>
2021-01-10 19:09:50 -0300

    Finish front-web

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -3,10 +3,15 @@ import './App.css';
 import Navbar from './Navbar';
 import Routes from './Routes';
 
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
   return (
-    <Routes/>
+    <>
+      <Routes/>
+      <ToastContainer/>
+    </>
   );
 }
 
