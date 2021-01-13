50686b02462ff09d5f248106d634b0ecd6dc56cb
Victor Pimenta <victorpimenta.ti@gmail.com>
2021-01-13 18:54:52 -0300

    Create index and add assets

---
+++ front-mobile/src/Header/index.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { StyleSheet, Text, View, Image } from 'react-native';
+
+function Header() {
+  return (
+    <View style={styles.container}>
+      <Image source={require('../assets/logo.png')}/>
+      <Text style={styles.text}>DS Delivery!</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+      backgroundColor: '#DA5C5C',
+      height: 90,
+      paddingTop: 50,
+      flexDirection: 'row',
+      justifyContent: 'center'
+  },
+  text: {
+      
+  }
+});
+
+export default Header;
