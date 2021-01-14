8203a6b4d6f074045ba8611e9bc5c6c960000ad9
Victor Pimenta <victorpimenta.ti@gmail.com>
2021-01-14 19:18:47 -0300

    Add Orders page

---
+++ front-mobile/src/Orders/index.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { StyleSheet, Text, View, Image } from 'react-native';
+import { RectButton } from 'react-native-gesture-handler';
+import Header from '../Header';
+
+function Orders() {
+
+  const handleOnPress = () => {
+
+  }
+
+  return (
+    <>
+      <Header/>
+      <View >
+        <Text>Orders</Text>
+      </View>
+    </>
+  );
+}
+
+const styles = StyleSheet.create({});
+
+export default Orders;
