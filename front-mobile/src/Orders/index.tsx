61309c85f04512e61bbc2be710378f429ba15a87
Victor Pimenta <victorpimenta.ti@gmail.com>
2021-01-15 18:49:02 -0300

    Add Orders list

--- front-mobile/src/Orders/index.tsx
+++ front-mobile/src/Orders/index.tsx
@@ -1,24 +1,33 @@
 import React from 'react';
-import { StyleSheet, Text, View, Image } from 'react-native';
-import { RectButton } from 'react-native-gesture-handler';
+import { StyleSheet, ScrollView, View, Image } from 'react-native';
 import Header from '../Header';
+import OrderCard from '../OrderCard';
 
 function Orders() {
 
-  const handleOnPress = () => {
-
-  }
+//  const handleOnPress = () => {
+//  }
 
   return (
     <>
       <Header/>
-      <View >
-        <Text>Orders</Text>
-      </View>
+      <ScrollView style={styles.container}>
+        <OrderCard/>
+        <OrderCard/>
+        <OrderCard/>
+        <OrderCard/>
+        <OrderCard/>
+      </ScrollView>
     </>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    paddingRight: '5%',
+    paddingLeft: '5%',
+
+  }
+});
 
 export default Orders;
